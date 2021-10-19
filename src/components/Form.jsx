465973cbc54120b153455789522a9847b42087f2
Pranjal Goyal <id4pranjal@gmail.com>
2021-10-19 18:25:52 +0530

    intial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <form>
+            <label>Username</label>
+            <input type="text" ></input>
+            <label>Age</label>
+            <input type="number"></input>
+            <button>Add User</button>
+        </form>
+    )
+}
+
+export default Form
